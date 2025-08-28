76c816595a984a50d62e4bd4e4e8fdc516eff4a4
shadin khan <13307041@iubat.edu>
2025-08-28 23:47:28 +0600

    update Main Section part in index and heart Count in JS

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
-console.log("Hello Conntected")
+// console.log("Hello Conntected")
+
+//  Heart Count Part Start
+
+function heartCount() {
+  // star mark Count start
+  document
+    .getElementById("parent-card")
+    .addEventListener("click", function (e) {
+      // console.log(e.target)
+
+      if (e.target.className.includes("heart-icon")) {
+        // alert("card clicked");
+        let heart = parseInt(document.getElementById("heart").innerText);
+        // console.log(heart)
+        document.getElementById("heart").innerText = heart + 1;
+      }
+    });
+}
+
+//  Heart Count Part End
+
+//Make Cart Part Start
+
+
+
+//Make Cart Part Start
+
+// Function Calling Part Start 
+
+heartCount();
+
+
+// Function Calling Part End
